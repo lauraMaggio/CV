f8df79f210ef55c61cce6298952af9357319f133
Riccardus05 <89735121+Riccardus05@users.noreply.github.com>
2023-03-11 16:14:05 +0100

    test

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -92,7 +92,7 @@ Le principali mansioni svolte:
   experience: [
     {
       header: 'Performance',
-      skills: ['theater', 'play'],
+      skills: ['theater'],
       contents: `
 Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
 `
